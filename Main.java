9ca3982ac4237902ad1c859cbbe05f3e0fb52f11
blisn8405 <blisn8405@wrdsb.ca>
2020-10-01 16:12:10 +0000

    finished q3

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  *
  * @author 
@@ -9,7 +11,28 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create a scanner for user input
+Scanner input = new Scanner(System.in); 
+ // get the user grade between A to F
+ System.out.println("Please make a selection (A-F)");
+ String choice = input.nextLine();
+ //  switch case statement
+ switch(choice){
+   case "A":
+   System.out.println("This is between 80% and 100%");
+   break;
+   case "B":
+   System.out.println("This is between 70% and 79%");
+   break;
+   case "C":
+   System.out.println("This is between 60% and 69%");
+   break;
+   case "D":
+   System.out.println("This is between 50% and 59%");
+    case "F":
+   System.out.println("This is between 0% and 49%");
+ }
+   
+
   }
 }
